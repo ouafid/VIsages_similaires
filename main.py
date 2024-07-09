a7311968acdaee50c8e2ee50cfe7b6bacecfdba2
ouafid <mohamedhabchi@teccart.online>
2024-07-09 02:41:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ async def Faces(uploaded_file: UploadFile = File(...)):
     return {"similar_faces": similar_images_paths}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.1', port=8000)
